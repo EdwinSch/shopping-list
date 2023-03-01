dc700cd41f0fb852cc26d5b1156d1e4f92f1ddd3
EdwinSch <edwinschutjes@gmail.com>
2023-03-01 21:40:04 +0100

    setup first logic and working on alerts

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
 /* ---- TARGETS && INITIALIZERS ---- */
+const alert = document.querySelector(".alert");
+const form = document.querySelector(".grocery-form");
+const getItem = document.getElementById("grocery-item");
+const submitBtn = document.querySelector(".submit-btn");
+const itemsContainer = document.querySelector(".items-container");
+const itemsList = document.querySelector(".items-list");
+const clearBtn = document.querySelector(".clear-btn");
+
+// edit option
+let editElement;
+let editFlag = false;
+let editID = "";
 
 /* ---- FUNCTIONS ---- */
 
-console.log("hello world");
+// Submit Input from form
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  // get form input value
+  const value = getItem.value;
+  // get unique ID
+  const itemID = createID();
+  // check input case
+  if (value !== "" && editFlag === false) {
+    console.log("add item to the list");
+  } else if (value !== "" && editFlag === true) {
+    console.log("edit mode");
+  } else {
+    alert.classList.add("alert-danger");
+    alert.textContent = "empty input";
+  }
+});
+
+// Create unique ID for list items
+function createID() {
+  let int;
+  let id = "";
+  for (i = 0; i < 6; i++) {
+    int = Math.floor(Math.random() * 10);
+    id += int;
+  }
+  return id;
+}
