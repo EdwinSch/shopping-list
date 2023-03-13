c0b0ff2f6b9c64881d0f9e4dbcb75df62b7d0566
front-ed <edwinschutjes@gmail.com>
2023-03-13 21:20:37 +0100

    add local storage functionality

--- README.md
+++ README.md
@@ -6,6 +6,11 @@
 
 This practice project is a little twist on the classic ToDo List. Made with JavaScript.
 
+### Version 1.1.0
+
+- Local storage functionality added, existing list loads on refresh or page load.
+- Add, edit or remove updates local storage
+
 ### Version 1.0.0
 
 - Add, delete and edit items on the list.
--- script.js
+++ script.js
@@ -14,6 +14,9 @@ let editID = "";
 
 /* ---- SCRIPT ---- */
 
+// Load items from storage
+window.addEventListener("DOMContentLoaded", setupItems);
+
 // Submit Input from form
 form.addEventListener("submit", function (event) {
   event.preventDefault();
@@ -24,12 +27,37 @@ form.addEventListener("submit", function (event) {
   // check input case
   if (value !== "" && editFlag === false) {
     // create and add new item
-    const newItem = document.createElement("article");
-    newItem.classList.add("item");
-    const dataAttribute = document.createAttribute("data-id");
-    dataAttribute.value = itemID;
-    newItem.setAttributeNode(dataAttribute);
-    newItem.innerHTML = `
+    createListItem(itemID, value);
+    // diplay success alert
+    displayAlert("alert-success", "item added");
+
+    // add to local storage
+    addToLocalStorage(itemID, value);
+    // reset input field
+    resetDefault();
+  } else if (value !== "" && editFlag === true) {
+    editElement.innerHTML = value;
+    displayAlert("alert-success", "item editted");
+    editLocalStorage(editID, value);
+    resetDefault();
+  } else {
+    displayAlert("alert-danger", "empty input");
+  }
+});
+
+// Clear ALL list items
+clearBtn.addEventListener("click", clearItems);
+
+/* ---- FUNCTIONS ---- */
+
+// Create list item
+function createListItem(itemID, value) {
+  const newItem = document.createElement("article");
+  newItem.classList.add("item");
+  const dataAttribute = document.createAttribute("data-id");
+  dataAttribute.value = itemID;
+  newItem.setAttributeNode(dataAttribute);
+  newItem.innerHTML = `
      <p class="title">${value}</p>
             <div class="btn-container">
               <button type="button" class="edit-btn">
@@ -39,34 +67,16 @@ form.addEventListener("submit", function (event) {
                 <i class="fa-sharp fa-solid fa-trash"></i>
               </button>
             </div>`;
-    // append the new item to the list
-    itemsList.appendChild(newItem);
-    // diplay success alert
-    displayAlert("alert-success", "item added");
-    // Get delete item button and add event listener
-    const deleteBtn = newItem.querySelector(".delete-btn");
-    deleteBtn.addEventListener("click", deleteItem);
-    // Get edit item button and add event listener
-    const editBtn = newItem.querySelector(".edit-btn");
-    editBtn.addEventListener("click", editItem);
-    // add to local storage
+  // append the new item to the list
+  itemsList.appendChild(newItem);
 
-    // reset input field
-    resetDefault();
-  } else if (value !== "" && editFlag === true) {
-    editElement.innerHTML = value;
-    displayAlert("alert-success", "item editted");
-    // edit local storage
-    resetDefault();
-  } else {
-    displayAlert("alert-danger", "empty input");
-  }
-});
-
-// Clear List
-clearBtn.addEventListener("click", clearItems);
-
-/* ---- FUNCTIONS ---- */
+  // Get delete item button and add event listener
+  const deleteBtn = newItem.querySelector(".delete-btn");
+  deleteBtn.addEventListener("click", deleteItem);
+  // Get edit item button and add event listener
+  const editBtn = newItem.querySelector(".edit-btn");
+  editBtn.addEventListener("click", editItem);
+}
 
 // Create unique ID for list items
 function createID() {
@@ -112,10 +122,11 @@ function editItem(event) {
 function deleteItem(event) {
   const targetElement = event.currentTarget.parentElement.parentElement;
   const targetId = targetElement.dataset.id;
-  console.log(targetId);
+  // console.log(targetId);
   itemsList.removeChild(targetElement);
   displayAlert("alert-success", "item deleted");
   resetDefault();
+  removeFromLocalStorage(targetId);
 }
 
 // clear items function
@@ -126,6 +137,7 @@ function clearItems() {
       itemsList.removeChild(item);
       displayAlert("alert-success", "all items deleted");
       resetDefault();
+      localStorage.removeItem("list");
     });
   } else {
     displayAlert("alert-danger", "no items on list");
@@ -133,3 +145,47 @@ function clearItems() {
 }
 
 /* ---- LOCAL STORAGE FUNCTIONS ---- */
+
+function setupItems() {
+  let items = getLocalStorage();
+  if (items.length > 0) {
+    items.forEach(function (item) {
+      createListItem(item.id, item.value);
+    });
+  }
+}
+
+function addToLocalStorage(id, value) {
+  const item = { id: id, value: value };
+  let items = getLocalStorage();
+
+  items.push(item);
+  localStorage.setItem("list", JSON.stringify(items));
+}
+
+function getLocalStorage() {
+  return localStorage.getItem("list")
+    ? JSON.parse(localStorage.getItem("list"))
+    : [];
+}
+
+function removeFromLocalStorage(id) {
+  let items = getLocalStorage();
+  items = items.filter(function (item) {
+    if (item.id !== id) {
+      return item;
+    }
+  });
+  localStorage.setItem("list", JSON.stringify(items));
+}
+
+function editLocalStorage(id, value) {
+  let items = getLocalStorage();
+  items = items.map(function (item) {
+    if (item.id === id) {
+      item.value = value;
+    }
+    return item;
+  });
+  localStorage.setItem("list", JSON.stringify(items));
+}
